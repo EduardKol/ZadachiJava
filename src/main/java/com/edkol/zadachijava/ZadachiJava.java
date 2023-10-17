498ba4afb77d9ba57bec832b311dbe73e77f38e2
EduardKol <eduardkolesnikov@mail.ru>
2023-10-17 17:52:04 +0300

    first

---
+++ src/main/java/com/edkol/zadachijava/ZadachiJava.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edkol.zadachijava;
+
+import com.edkol.zadachijava.BasicJava.Arrays;
+import com.edkol.zadachijava.BasicJava.BasicFunctionStrings;
+import com.edkol.zadachijava.BasicJava.Lists;
+import com.edkol.zadachijava.BasicJava.TypeData;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Eduard
+ */
+public class ZadachiJava {
+
+    public static void main(String[] args) {
+        try {
+            Arrays.getCub2();
+        } catch (Exception ex) {
+            Logger.getLogger(ZadachiJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
